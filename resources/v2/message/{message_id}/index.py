d6bdc035ea4822c679b54b1e9ba8d99db3cfbddc
ChandanChainani <chainanichan@gmail.com>
2021-11-04 15:18:55 +0530

    added url parameter example

--- resources/v2/message/{message_id}/index.py
+++ resources/v2/message/{message_id}/index.py
@@ -3,10 +3,10 @@ class Index:
     def after_validation(self, req, res):
         print("message v2 after_validation")
 
-    def on_get(self, req, res):
+    def on_get(self, req, res, message_id):
         """Handles GET requests"""
         data = {
-            "message": "message v2"
+            "message": f"message v2 for {message_id}"
         }
 
         res.media = data
